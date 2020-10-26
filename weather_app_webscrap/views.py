767a24155ad0f1bcbb80fae34e18299b93b959ff
Mpanshuman <m.p.ansuman10@gmail.com>
2020-10-26 23:03:01 +0530

    Added comments

--- weather_app_webscrap/views.py
+++ weather_app_webscrap/views.py
@@ -6,6 +6,8 @@ from bs4 import BeautifulSoup
 # Create your views here.
 
 def get_htmlpage(city):
+    
+    # We use this code so that google will think us as browser...
     USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
     LANGUAGE = "en-US,en;q=0.5"
     session = requests.Session()
@@ -14,12 +16,15 @@ def get_htmlpage(city):
     session.headers['Content-Language'] = LANGUAGE
     city = city.replace(' ','+')
     html_page= session.get(f"https://www.google.com/search?q=weather+{city}").text
+    
     return html_page
 
 
 
 def index(request):
+    
     weather = None
+    
     if 'city' in request.GET:
         city = request.GET.get('city')
         html_page = get_htmlpage(city)
@@ -29,6 +34,5 @@ def index(request):
         weather['daytime'] = soup.find('div', attrs={'id':'wob_dts'}).text
         weather['sky'] = soup.find('span', attrs={'id':'wob_dc'}).text
         weather['temp'] = soup.find('span', attrs={'id':'wob_tm'}).text
-
 
     return render(request,'core/index.html',{'weather':weather})
